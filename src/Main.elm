3bb4df21b8debbce212dbc574c1017b403753334
Bryan Jennings <bryanjenningz@gmail.com>
2022-10-05 20:37:15 -0700

    Add video cards

--- src/Main.elm
+++ src/Main.elm
@@ -4,21 +4,96 @@ import Browser
 import Html exposing (Html)
 import Html.Attributes exposing (class)
 import List.Extra as List
+import Material.Button as Button
+import Material.Card as Card
 import Material.Tab as Tab exposing (Tab)
 import Material.TabBar as TabBar
 
 
+type alias TabData =
+    { text : String
+    , icon : String
+    , content : Model -> Html Msg
+    }
+
+
+listenTabIndex : Int
+listenTabIndex =
+    1
+
+
+firstTab : TabData
+firstTab =
+    { text = "Find"
+    , icon = "search"
+    , content = \_ -> Html.div [] (List.map viewVideoCard videos)
+    }
+
+
+remainingTabs : List TabData
+remainingTabs =
+    [ { text = "Listen"
+      , icon = "headphones"
+      , content =
+            \model ->
+                Html.text
+                    (case model.selectedVideo of
+                        Nothing ->
+                            "No video selected"
+
+                        Just videoId ->
+                            "Video ID: " ++ videoId
+                    )
+      }
+    , { text = "Review"
+      , icon = "grading"
+      , content = \_ -> Html.text "Review"
+      }
+    , { text = "Profile"
+      , icon = "person"
+      , content = \_ -> Html.text "Profile"
+      }
+    ]
+
+
+type alias VideoData =
+    { id : String
+    , title : String
+    }
+
+
+videos : List VideoData
+videos =
+    [ { id = "UwRZ8TY2Z4k"
+      , title = "It's too hot this summer! Talk about our environment 今年夏天也太热了吧聊聊环保"
+      }
+    , { id = "jKPlNHHYKZo"
+      , title = "Difference between giving Chinese and English names 中英文起名字的区别"
+      }
+    , { id = "8FKWqzd5jjs"
+      , title = "Talk about insomnia 失眠和晚睡强迫症"
+      }
+    ]
+
+
 type alias Model =
-    { selectedTab : Int }
+    { selectedTab : Int
+    , selectedVideo : Maybe String
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { selectedTab = 0 }, Cmd.none )
+    ( { selectedTab = 0
+      , selectedVideo = Nothing
+      }
+    , Cmd.none
+    )
 
 
 type Msg
     = TabClicked Int
+    | ListenToVideo String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -27,24 +102,13 @@ update msg model =
         TabClicked selectedTab ->
             ( { model | selectedTab = selectedTab }, Cmd.none )
 
-
-type alias TabData =
-    { text : String
-    , icon : String
-    }
-
-
-firstTab : TabData
-firstTab =
-    { text = "Find", icon = "search" }
-
-
-remainingTabs : List TabData
-remainingTabs =
-    [ { text = "Listen", icon = "headphones" }
-    , { text = "Review", icon = "grading" }
-    , { text = "Profile", icon = "person" }
-    ]
+        ListenToVideo videoId ->
+            ( { model
+                | selectedTab = listenTabIndex
+                , selectedVideo = Just videoId
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
@@ -55,8 +119,7 @@ view model =
             [ (firstTab :: remainingTabs)
                 |> List.getAt model.selectedTab
                 |> Maybe.withDefault firstTab
-                |> .text
-                |> Html.text
+                |> (\tab -> tab.content model)
             ]
         ]
 
@@ -76,6 +139,31 @@ viewTab model index tab =
             |> Tab.setOnClick (TabClicked index)
         )
         { label = tab.text, icon = Just (Tab.icon tab.icon) }
+
+
+viewVideoCard : VideoData -> Html Msg
+viewVideoCard video =
+    Card.card (Card.config |> Card.setAttributes [ class "px-4 pt-4 mb-4" ])
+        { blocks =
+            ( Card.block <|
+                Html.div []
+                    [ Html.h2 [ class "text-xl" ] [ Html.text video.title ]
+                    ]
+            , []
+            )
+        , actions =
+            Just <|
+                Card.actions
+                    { buttons =
+                        [ Card.button
+                            (Button.config
+                                |> Button.setOnClick (ListenToVideo video.id)
+                            )
+                            "Listen"
+                        ]
+                    , icons = []
+                    }
+        }
 
 
 subscriptions : Model -> Sub Msg
